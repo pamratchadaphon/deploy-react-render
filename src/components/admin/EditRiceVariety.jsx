9aaae7e80efa753f47710ff13297776c61f6d2c4
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-31 18:22:23 +0700

    first commit

---
+++ src/components/admin/EditRiceVariety.jsx
@@ -0,0 +1,370 @@
+import { useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { motion } from "framer-motion";
+
+const EditRiceVariety = ({ id }) => {
+  const [modal, setModal] = useState(false);
+  const [imageURL, setImageURL] = useState("");
+  const [resImg, setResImg] = useState("");
+  const [image, setImage] = useState(null);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [yield_rice, setYield] = useState("");
+  const [height, setHeight] = useState("");
+  const [photosensitivity, setPhotosensitivity] = useState("");
+  const [stability, setStability] = useState("");
+  const [precautions, setPrecautions] = useState("");
+  const [type, setType] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleModal = () => {
+    setModal(!modal);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://deploy-nodejs-render.onrender.com/riceVariety/${id}`
+        );
+        setResImg(res.data.image);
+        setName(res.data.name);
+        setAge(res.data.age);
+        setHeight(res.data.height);
+        setPhotosensitivity(res.data.photosensitivity);
+        setPrecautions(res.data.precautions);
+        setStability(res.data.stability);
+        setYield(res.data.yield);
+        setImage(res.data.image);
+        setType(res.data.type);
+      } catch (error) {
+        console.log("Error" + error);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  const clickChangeImg = (e) => {
+    setImage(e.target.files[0]);
+    setImageURL(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const uploadImage = async () => {
+    if (!image) return "";
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", import.meta.env.VITE_UPLOAD_PRESET);
+    formData.append("cloud_name", "dwskhfx91");
+
+    try {
+      const response = await fetch(
+        `https://api.cloudinary.com/v1_1/dwskhfx91/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (!response.ok) throw new Error("Failed to upload image");
+      const imgData = await response.json();
+      return imgData.url.toString();
+    } catch (error) {
+      console.log(error);
+      return "";
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      let updatedImageURL = imageURL;
+      if (image && image !== resImg) {
+        updatedImageURL = await uploadImage();
+      } else {
+        updatedImageURL = resImg;
+      }
+
+      const formData = {
+        name,
+        age,
+        yield: yield_rice,
+        height,
+        photosensitivity,
+        stability,
+        precautions,
+        type,
+        image: updatedImageURL,
+      };
+
+      await axios
+        .put(
+          `https://deploy-nodejs-render.onrender.com/riceVariety/edit/${id}`,
+          formData
+        )
+        .then((result) => console.log(result.data));
+
+      Swal.fire({
+        title: "แก้ไขสำเร็จ",
+        icon: "success",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        className="flex justify-center items-center"
+        onClick={handleModal}
+      >
+        <div className="hover:bg-orange-400 rounded-md bg-orange-100 text-orange-500 hover:text-white w-8 h-8 flex justify-center items-center border border-orange-200">
+          <FaRegEdit className="w-5 h-5" />
+        </div>
+      </button>
+
+      {modal ? (
+        <div className="overflow-x-hidden overflow-y-auto fixed top-0 right-0 left-0 z-50 flex justify-center items-center bg-black bg-opacity-50 h-screen">
+          <div className="relative p-4 w-full max-w-3xl max-h-full">
+            <motion.div
+              initial={{ y: -100 }}
+              animate={{ y: 0 }}
+              transition={{ duration: 1 }}
+              className="relative bg-white rounded-lg shadow"
+            >
+              <div className="flex justify-between items-center p-4 md:p-5 border-b rounded-t">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  แก้ไขพันธุ์ข้าว
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 rounded-lg w-8 h-8 ms-auto inline-flex justify-center items-center"
+                  onClick={handleModal}
+                >
+                  <IoMdClose className="w-10 h-10" />
+                </button>
+              </div>
+              <form
+                className="p-4 md:p-5 flex flex-col space-y-4"
+                encType="multipart/form-data"
+                onSubmit={handleSubmit}
+              >
+                <div className="grid md:grid-cols-2 gap-4">
+                  <div className="flex items-center text-gray-900 text-sm">
+                    <label
+                      htmlFor="name"
+                      className="font-medium mb-2 w-1/4 text-start"
+                    >
+                      ชื่อพันธุ์
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                    />
+                  </div>
+                  <div className="flex items-center text-gray-900 text-sm">
+                    <label
+                      htmlFor="yield"
+                      className="font-medium mb-2 w-1/4 text-start"
+                    >
+                      ผลผลิต
+                    </label>
+                    <input
+                      type="text"
+                      name="yield"
+                      id="yield"
+                      value={yield_rice}
+                      onChange={(e) => setYield(e.target.value)}
+                      required
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                    />
+                    กก./ไร่
+                  </div>
+                  <div className="flex items-center text-gray-900 text-sm">
+                    <label
+                      htmlFor="type"
+                      className="font-medium mb-2 w-1/4 text-start"
+                    >
+                      ชนิด
+                    </label>
+                    <select
+                      name="type"
+                      id="type"
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                      onChange={(e) => setType(e.target.value)}
+                      value={type}
+                      required
+                    >
+                      <option value="">เลือกชนิด</option>
+                      <option value="ข้าวเจ้า">ข้าวเจ้า</option>
+                      <option value="ข้าวเหนียว">ข้าวเหนียว</option>
+                    </select>
+                  </div>
+                  <div className="flex items-center text-gray-900 text-sm text-start">
+                    <label
+                      htmlFor="photosensitivity"
+                      className="font-medium mb-2 w-1/4"
+                    >
+                      ความไวแสง
+                    </label>
+                    <select
+                      name="photosensitivity"
+                      id="photosensitivity"
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                      onChange={(e) => setPhotosensitivity(e.target.value)}
+                      value={photosensitivity}
+                      required
+                    >
+                      <option value="">เลือกความไวแสง</option>
+                      <option value="ไวต่อช่วงแสง">ไวต่อช่วงแสง</option>
+                      <option value="ไม่ไวต่อช่วงแสง">ไม่ไวต่อช่วงแสง</option>
+                    </select>
+                  </div>
+                  <div className="flex items-center text-gray-900 text-sm">
+                    <label
+                      htmlFor="age"
+                      className="font-medium  mb-2 w-1/4 text-start"
+                    >
+                      อายุเก็บเกี่ยว
+                    </label>
+                    <input
+                      type="text"
+                      name="age"
+                      id="age"
+                      value={age}
+                      onChange={(e) => setAge(e.target.value)}
+                      required
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                    />
+                  </div>
+                  <div className="flex items-center text-gray-900 text-sm">
+                    <label
+                      htmlFor="height"
+                      className="font-medium  mb-2 w-1/4 text-start"
+                    >
+                      ความสูงเฉลี่ย
+                    </label>
+                    <input
+                      type="text"
+                      name="height"
+                      id="height"
+                      value={height}
+                      onChange={(e) => setHeight(e.target.value)}
+                      required
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 w-1/3 md:w-2/4"
+                    />
+                    ซม.
+                  </div>
+                  <div className="flex text-sm text-gray-900">
+                    <label
+                      htmlFor="stability"
+                      className="font-medium  mb-2 w-1/4 text-start"
+                    >
+                      ลักษณะเด่น
+                    </label>
+                    <textarea
+                      type="text"
+                      name="stability"
+                      id="stability"
+                      value={stability}
+                      onChange={(e) => setStability(e.target.value)}
+                      required
+                      className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 mr-2 h-24 w-1/3 md:w-2/4"
+                    ></textarea>
+                  </div>
+                  <div className="flex text-gray-900 text-sm">
+                    <label
+                      htmlFor="precautions"
+                      className="font-medium w-1/4 text-start"
+                    >
+                      ข้อควรระวัง
+                    </label>
+                    <textarea
+                      type="text"
+                      name="precautions"
+                      id="precautions"
+                      required
+                      value={precautions}
+                      onChange={(e) => setPrecautions(e.target.value)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 h-24 w-1/3 md:w-2/4"
+                    ></textarea>
+                  </div>
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="image"
+                      className="text-sm font-medium text-gray-900 mb-2 mr-2 w-1/4 text-start"
+                    >
+                      รูปภาพใหม่
+                    </label>
+                    <input
+                      type="file"
+                      name="image"
+                      id="image"
+                      onChange={clickChangeImg}
+                      className="text-gray-900 text-sm rounded-lg py-2.5 w-3/4"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex justify-center text-gray-900">
+                  {imageURL ? (
+                    <div>
+                      แสดงรูปภาพใหม่
+                      <img src={imageURL} className="w-72 h-48 pt-2" />
+                    </div>
+                  ) : (
+                    <div>
+                      แสดงรูปภาพเดิม
+                      <img src={resImg} className="w-72 h-48 pt-2" />
+                    </div>
+                  )}
+                </div>
+
+                <div className="space-x-2 flex justify-end items-center">
+                  <button
+                    type="submit"
+                    className="text-sm bg-green-600 py-3 px-4 rounded-md text-white hover:bg-green-100 hover:text-green-700 hover:duration-200"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "กำลังบันทึก..." : "บันทึก"}
+                  </button>
+                  <button
+                    type="button"
+                    className="p-3 bg-slate-50 rounded-md text-sm border hover:bg-gray-100"
+                    onClick={handleModal}
+                  >
+                    ยกเลิก
+                  </button>
+                </div>
+              </form>
+            </motion.div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+EditRiceVariety.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default EditRiceVariety;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,186 @@
+import axios from "axios";
+import { GoArrowRight } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { ReactTyped } from "react-typed";
+import { useEffect, useState } from "react";
+import { RiErrorWarningLine } from "react-icons/ri";
+
+const Login = () => {
+  const navigator = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [email, setEmail] = useState(false);
+  const [password, setPassword] = useState(false);
+
+  const check = () => {
+    values.email === "" ? setEmail(true) : null;
+    values.password === "" ? setPassword(true) : null;
+  };
+
+  useEffect(() => setEmail(false), [values.email]);
+  useEffect(() => setPassword(false), [values.password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    check();
+
+    if (values.email !== "" && values.password !== "") {
+      try {
+        setIsLoading(true);
+        const response = await axios.post(
+          "https://deploy-nodejs-render.onrender.com/user/login",
+          values
+        );
+        const id = response.data.user_id;
+        if (response.data.role === "admin") {
+          localStorage.setItem("token", response.data.token);
+          navigator(`/admin/dashboard`);
+        } else {
+          localStorage.setItem("token", response.data.token);
+          navigator(`/user/home/${id}`);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        if (error.response.data.error === "Incorrect password") {
+          Swal.fire({
+            title: "รหัสผ่านไม่ถูกต้อง",
+            icon: "error",
+          });
+        } else if (error.response.data.error === "User not found") {
+          Swal.fire({
+            title: "ยังไม่มีบัญชี",
+            text: "กรุณาสร้างบัญชีก่อนลงชื่อเข้าใช้",
+            icon: "error",
+          });
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  return (
+    <div className="flex h-screen w-screen bg-gradient-to-r from-green-50 to-sky-50">
+      <div className="w-full flex flex-col md:flex-row items-center">
+        <div className="w-full h-1/3 flex items-center justify-center">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-green-500 md:text-5xl lg:text-6xl">
+            <ReactTyped
+              strings={[
+                "จดบันทึกการปลูกข้าว",
+                "ตรวจสอบผลผลิต",
+                "เลือกพันธุ์ข้าว",
+                "ดูรายงานแปลงนา",
+                "ดูรายงานค่าใช้จ่าย",
+              ]}
+              typeSpeed={100}
+              loop
+              backSpeed={50}
+              showCursor={false}
+            />
+          </h1>
+        </div>
+        <div className="w-full flex justify-center items-center bg-white h-screen">
+          <div className="flex flex-col p-6 md:p-4 w-full md:w-2/3 h-96 justify-center">
+            {isLoading ? (
+              <div className="p-4 mb-8 text-sm text-blue-800 rounded-lg bg-blue-50 flex items-center justify-center">
+                <svg
+                  aria-hidden="true"
+                  className="inline w-5 h-5 text-gray-200 animate-spin  fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="font-medium ml-3">กำลังเข้าสู่ระบบ...</span>
+              </div>
+            ) : null}
+            <h3 className="text-xl font-semibold text-gray-900 text-center">
+              ลงชื่อเข้าใช้
+            </h3>
+            <form
+              className="space-y-10 flex flex-col justify-start"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  อีเมล
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={(e) =>
+                    setValues({ ...values, email: e.target.value })
+                  }
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full block w-full p-2.5 hover:border-green-700 hover:border-2"
+                />
+                {email ? (
+                  <div className="text-sm text-red-500 flex items-center gap-1">
+                    <RiErrorWarningLine />
+                    <span>กรุณากรอกอีเมล</span>
+                  </div>
+                ) : null}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  รหัสผ่าน
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={(e) =>
+                    setValues({ ...values, password: e.target.value })
+                  }
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full hover:border-green-700 hover:border-2 w-full p-2.5"
+                />
+                {password ? (
+                  <div className="text-sm text-red-500 flex items-center gap-1">
+                    <RiErrorWarningLine />
+                    <span>กรุณากรอกรหัสผ่าน</span>
+                  </div>
+                ) : null}
+              </div>
+              <button
+                type="submit"
+                className="w-full md:w-full px-5 py-2.5 text-base font-medium text-center text-white bg-green-600 hover:bg-green-100 hover:text-green-700 hover:duration-500 rounded-full flex items-center justify-center gap-2 shadow"
+              >
+                <span>เข้าใช้งาน</span>
+                <GoArrowRight />
+              </button>
+            </form>
+            <div className="flex justify-end items-center pt-4">
+              <div className="text-sm font-medium text-gray-900 flex space-x-2">
+                <div>ยังไม่มีบัญชี? </div>
+                <a
+                  href="/register"
+                  className="text-blue-600 hover:underline cursor:pointer animate-bounce"
+                >
+                  สร้างบัญชี
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
